846bb7efc8fe664e0e92466abc5ba1d051b48032
Chimobi Okengwu <chimobi.okengwu01@gmail.com>
2021-05-21 16:21:15 +0100

    Added my Frontend_Algorithms_Task

---
+++ Frontend_Algorithms_Task.js
@@ -0,0 +1,29 @@
+// Degree Fahrenheit to Degree Celsuius converter
+// function convertFahrToCelsius(tempInFahr) {
+//     let F = tempInFahr;
+    
+//     if ((Number(F) >= 0) || (Number(F) <= 0) === 'true') {  
+//        var C = (F - 32) * 5/9
+//        return C.toFixed(4)
+//     }else {
+//     return `${tempInFahr} is not a valid number but a/an ${typeof(tempInFahr)}.`
+//     }
+// }
+
+// console.log(convertFahrToCelsius(0))
+// console.log(convertFahrToCelsius('0'))
+// console.log(convertFahrToCelsius([1,2,3]));
+// console.log(convertFahrToCelsius({temp: 0}));
+
+
+// checkYuGiOh
+function checkYuGiOh(positiveNum) {
+   listOfNumbers = array(arange(i=1, i++, n=positiveNum));
+   return listOfNumbers
+}
+
+console.log(checkYuGiOh(10));
+
+
+
+
